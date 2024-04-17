aeaa08b0f17ffa7c2f135f90a27c6727aa52f619
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-04-17 17:43:06 +0000

    add forEach method, verify state of button and disable btn according to state

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,39 +77,52 @@ const startQuiz = () => {
 const showQuestion = () => {
   let currentQuestion = questions[currentQuestionIndex]; //this will get us the question index, like questions[0] = questions[Q1]
   // console.log(currentQuestion);
-  let questionNo = currentQuestionIndex + 1; 
-  questionElement.innerHTML = questionNo + ". "+ currentQuestion.question; // to display our question inside the h2 html element.
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question; // to display our question inside the h2 html element.
 
-  currentQuestion.answers.forEach((answer) => {     // this will go through the answers array using forEach method. 
+  currentQuestion.answers.forEach((answer) => {
+    // this will go through the answers array using forEach method.
     const button = document.createElement("button"); //create button tag and store it in button const
-    button.innerHTML = answer.text;  //we will write the text from our answers array using the answer variable from the forEach method.
+    button.innerHTML = answer.text; //we will write the text from our answers array using the answer variable from the forEach method.
     button.classList.add("btn"); //add a class to the new button
     answerBtn.appendChild(button); //display const button inside our answer-buttons div
 
-    if(answer.correct){ //
+    if (answer.correct) {
       button.dataset.correct = answer.correct;
     }
 
-    button.addEventListener("click", selectAnswer) //an event listener that will execute a function whenever button clicked.
+    button.addEventListener("click", selectAnswer); //an event listener that will execute a function whenever button clicked.
   });
 };
 
 // will hide the next btn until a answer is clicked.
 const reset = () => {
   nextBtn.style.display = "none";
-}
+  while(answerBtn.firstChild){
+    answerBtn.removeChild(answerBtn.firstChild);
+  }
+};
 
 // this function will check whether the answer is true or false and will add a css class to the button depending on the state.
 const selectAnswer = (e) => {
   const selectedBtn = e.target;
   const isCorrect = selectedBtn.dataset.correct === "true";
-  if(isCorrect){
-    selectedBtn.classList.add("correct")
-  }else{
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
     selectedBtn.classList.add("incorrect");
   }
-}
 
+  // this will verify the state of the button and assign a class accordingly, and disabled the other buttons when event is triggered.
+  Array.from(answerBtn.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextBtn.style.display = "block";
+};
 
 
 startQuiz();
