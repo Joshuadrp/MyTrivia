e1890debb802036fabe5c6db241778b4397a83a2
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-04-17 14:42:12 +0000

    add const elements in js file for quiestions and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+const questions = [
+    {
+        question: "Q1", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    }, 
+    
+    {
+        question: "Q2", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    },  
+
+    {
+        question: "Q3", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    },  
+
+    {
+        question: "Q4", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    },  
+
+    {
+        question: "Q5", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    },  
+
+    {
+        question: "Q6", 
+        answers:[
+            {text: "A1", correct:true},
+            {text: "A2", correct:false},
+            {text: "A3", correct:false},
+            {text: "A4", correct:false},
+
+        ]
+    }
+]
+
+const questionElement = document.getElementById("question");
+const answerBtn = document.getElementById("answer-buttons");
+const nextBtn = document.getElementById("next-btn");
+
